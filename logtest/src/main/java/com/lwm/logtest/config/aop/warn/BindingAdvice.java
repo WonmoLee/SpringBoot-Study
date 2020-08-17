78534424e2e6371a0836e5104e392023d2749fbe
wonmoLee <leewonmo94@gmail.com>
2020-08-17 14:34:38 +0900

    로그 예제 완성

--- logtest/src/main/java/com/lwm/logtest/config/aop/warn/BindingAdvice.java
+++ logtest/src/main/java/com/lwm/logtest/config/aop/warn/BindingAdvice.java
@@ -1,14 +1,54 @@
 package com.lwm.logtest.config.aop.warn;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 @Component
 @Aspect
 public class BindingAdvice {
 
-	public Object bindingPrint() {
+	
+	private static final Logger log = LoggerFactory.getLogger(BindingAdvice.class);
+
+	
+	@Around("execution(* com.lwm.logtest.controller..*Controller.*(..))")
+	public Object bindingPrint(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 		
-		return null;
+		Object[] args = proceedingJoinPoint.getArgs();
+		
+		String type = proceedingJoinPoint.getSignature().getDeclaringTypeName()+":";
+		String methodName = proceedingJoinPoint.getSignature().getName()+"() ";
+		
+		for(Object arg : args) {
+			if(arg instanceof BindingResult) {
+				BindingResult bindingResult = (BindingResult) arg;
+				if(bindingResult.hasErrors()) {
+					Map<String, String> errorMap = new HashMap<>();
+					for (FieldError error : bindingResult.getFieldErrors()) {
+						log.info("나찍혀?");
+						log.debug("디버그 찍혀?");
+						log.error("에러 찍허?");
+						log.warn(type+methodName+error.getDefaultMessage());
+						errorMap.put(error.getField(), error.getDefaultMessage());
+					}
+					
+					return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+					
+				}
+			}
+		}
+		
+		return proceedingJoinPoint.proceed();
 	}
 }
--- logtest/src/main/java/com/lwm/logtest/controller/IndexController.java
+++ logtest/src/main/java/com/lwm/logtest/controller/IndexController.java
@@ -1,14 +1,10 @@
 package com.lwm.logtest.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,15 +22,6 @@ public class IndexController {
 	
 	@PostMapping("/user")
 	public ResponseEntity<?> user(@Valid @RequestBody User user, BindingResult bindingResult) {
-		
-		if(bindingResult.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<>();
-			for (FieldError error : bindingResult.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-			
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
 		
 		return new ResponseEntity<User>(user, HttpStatus.CREATED);
 	}
